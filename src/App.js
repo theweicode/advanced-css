8600f89a7dd503b450d4e7634a82fee3a65e29b7
Will Ting <theweicode@gmail.com>
2019-05-31 13:56:37 -0700

    flexbox code

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="wrapper">
+    <div className="container">
+  <div className="wrapper">
     
-    <div className="box-1">Box-1</div>
-    <div className="box-2">Box-2</div>
-    <div className="box-3">Box-3</div>
-    <div className="box-4">Box-4     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-    <div className="box-5">Box-5</div>
-    <div className="box-6">Box-6</div>
+    <div className="box">Box 1</div>
+    <div className="box">Box 2</div>
+    <div className="box">Box 3</div>
+    <div className="box">Box 4</div>
+    <div className="box2">Box 5</div>
+    <div className="box3">Box 6</div>
+    <div className="box">Box 2</div>
+    <div className="box">Box 3</div>
+    <div className="box">Box 4</div>
+    <div className="box2">Box 5</div>
+    <div className="box3">Box 6</div>
+    <div className="box">Box 2</div>
+    <div className="box">Box 3</div>
+    <div className="box">Box 4</div>
+    <div className="box2">Box 5</div>
+    <div className="box3">Box 6</div>
     </div>
+
+    </div>
+  
   );
 }
 
